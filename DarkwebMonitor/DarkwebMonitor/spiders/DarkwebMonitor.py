fe38c934b90c3cc1c8e15fa599da33e3246d48a6
AdilKhan000 <139900944+AdilKhan000@users.noreply.github.com>
2024-03-02 22:33:22 +0530

    commit

--- DarkwebMonitor/DarkwebMonitor/spiders/DarkwebMonitor.py
+++ DarkwebMonitor/DarkwebMonitor/spiders/DarkwebMonitor.py
@@ -2,6 +2,8 @@ import scrapy
 from bs4 import BeautifulSoup
 from collections import Counter
 from string import punctuation
+import nltk
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk import PorterStemmer
 import re
@@ -18,31 +20,26 @@ def clean_text(text):
     filtered_words = [word for word in word_list if word not in STOPS]
     return filtered_words
 
+from nltk.tokenize import sent_tokenize
+
 def clean2(content):
     # Remove Tags
-    cleanr = re.compile('<.*?>')
-    cleantext = re.sub(cleanr, '', content)
+    soup = BeautifulSoup(content, "html.parser")
+    text_content = soup.get_text().replace('\n',' ')
 
-    # Special Character Free
-    cleantext.lower()
-    cleantext = cleantext.replace('\n', ' ').replace('\r', ' ').replace('\t', ' ').replace('  ', ' ')
-
-    # Remove Punctuations
-    cleantext = cleantext.translate(str.maketrans('', '', punctuation))
-
-    # Create Word List
-    word_list = cleantext.split(' ')
-
-    # Remove Stop words (English)
-    filtered_words = [word for word in word_list if word not in STOPS]
-
-    return dict(Counter(filtered_words))
-
+    # Tokenize into sentences
+    sentences = sent_tokenize(text_content)
+    return sentences
 
 class DrlSpider(scrapy.Spider):
     name = 'DRL'
     allowed_domains = ['onion']
-    start_urls = ['http://c32zjeghcp5tj3kb72pltz56piei66drc63vkhn5yixiyk4cmerrjtid.onion/f/guns/comments']
+    userInput = input('Enter the url:')
+    if userInput:
+        start_urls = [userInput]
+    else:
+        start_urls = ['http://c32zjeghcp5tj3kb72pltz56piei66drc63vkhn5yixiyk4cmerrjtid.onion/f/guns/comments']
+    
 
     def parse(self, response):
         soup = BeautifulSoup(response.body, "html.parser")
@@ -53,8 +50,10 @@ class DrlSpider(scrapy.Spider):
             if inspect_link and '.onion' in inspect_link:
                 links.update({inspect_link})
 
-        title = soup.title.text
+        title_tag = soup.title
+        title = title_tag.text if title_tag else ''
         title_keywords = Counter(clean_text(title))
+
 
         meta_tags = soup.find_all('meta')
 
@@ -78,6 +77,8 @@ class DrlSpider(scrapy.Spider):
         selector = Selector(text = response.text)
         comments = selector.xpath('//p/text()').extract()
 
+        Entire_page = clean2(response.text)
+        
         yield {
             'url': response.url,
             'title': title,
@@ -86,10 +87,9 @@ class DrlSpider(scrapy.Spider):
             'description': description,
             'meta': meta_data,
             'Data' : comments,
-            #'content' : clean2(response.text),
-
+            #'content' : Entire_page,
         }
-
+        print("Entire Page data tokens:",Entire_page)
         for next_page in links:
             if next_page:
                 yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
